a2e49b16f0f9c3de4965acdc55a7da832c29ba17
Anders Forssell <andersforssell@AF-MacBook-Air.local>
2014-06-23 01:18:14 +0200

    Initial Commit

---
+++ MIDIMacTest/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  MIDIMacTest
+//
+//  Created by Anders Forssell on 2014-06-23.
+//  Copyright (c) 2014 Anders Forssell. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
